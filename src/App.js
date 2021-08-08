64d21eeca05ea19612743a456c7048d4448ab86d
marikopa <mariko.passel@gmail.com>
2021-08-08 19:14:55 +0300

    added search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Dictionary app
+      <div className="container">
+        <header className="App-header">Dictionary app</header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
     </div>
   );
 }
