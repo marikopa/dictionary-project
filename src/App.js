5e62c37e8dc8cda50c3ad12d5a9a1e2897681152
marikopa <mariko.passel@gmail.com>
2021-08-08 16:02:09 +0300

    created the app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      Dictionary app
+    </div>
+  );
+}
+
+export default App;
